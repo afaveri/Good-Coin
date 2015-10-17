f00b26a4c79c5ca5c060e140a79acc946ed210d9
Alexandre Perozim de Faveri <afaveri@nat-oitwireless-inside-vapornet100-d-19869.Princeton.EDU>
2015-10-16 22:51:01 -0400

    Home Tab

--- Good Coin/Good Coin/GCHomeViewController.m
+++ Good Coin/Good Coin/GCHomeViewController.m
@@ -7,13 +7,77 @@
 //
 
 #import "GCHomeViewController.h"
+#import "GCRecentDonationsCollectionViewController.h"
+
+@interface GCHomeViewController ()
+
+- (void)createRecentDonationsCollectionView;
+
+@end
 
 @implementation GCHomeViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self.view setBackgroundColor:[UIColor blackColor]];
+    
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+
+    [self createRecentDonationsCollectionView];
+}
+
+- (void)createRecentDonationsCollectionView
+{
+    // Create collection view controller
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    [layout setItemSize:CGSizeMake(self.view.frame.size.height / 3.0 - 50.0, self.view.frame.size.height / 3.0 - 50.0)];
+    [layout setMinimumLineSpacing:self.view.frame.size.width / 10.0];
+    
+    GCRecentDonationsCollectionViewController *recentDonations = [[GCRecentDonationsCollectionViewController alloc]
+                                                                  initWithCollectionViewLayout:layout];
+    
+    // Add subview and child controller
+    [self addChildViewController:recentDonations];
+    [self.view addSubview:recentDonations.view];
+    [recentDonations didMoveToParentViewController:self];
+    
+    // Setup constraints
+    recentDonations.view.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:recentDonations.view
+                                                                     attribute:NSLayoutAttributeBottom
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.view
+                                                                     attribute:NSLayoutAttributeBottom
+                                                                    multiplier:1.0
+                                                                      constant:0.0]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:recentDonations.view
+                                                                     attribute:NSLayoutAttributeLeading
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.view
+                                                                     attribute:NSLayoutAttributeLeading
+                                                                    multiplier:1.0
+                                                                      constant:0.0]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:recentDonations.view
+                                                                     attribute:NSLayoutAttributeTrailing
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.view
+                                                                     attribute:NSLayoutAttributeTrailing
+                                                                    multiplier:1.0
+                                                                      constant:0.0]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:recentDonations.view
+                                                                     attribute:NSLayoutAttributeHeight
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.view
+                                                                     attribute:NSLayoutAttributeHeight
+                                                                    multiplier:1.0/3.0
+                                                                      constant:0.0]];
 }
 
 @end
---
+++ Good Coin/Good Coin/GCRecentDonationsCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  GCRecentDonationsCollectionViewCell.h
+//  Good Coin
+//
+//  Created by Alexandre Perozim de Faveri on 10/16/15.
+//  Copyright © 2015 Good Coin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCRecentDonationsCollectionViewCell : UICollectionViewCell
+
+@end
